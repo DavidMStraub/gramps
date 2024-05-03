b1d135be7a6f4c7fbbbfb021806fe5888b9362b4
David Straub <straub@protonmail.com>
2024-05-03 17:46:22 +0200

    Revert "Revert test_random"
    This reverts commit b7b8c8eeac88e47083448cb6d379296a8a3d1a6b.

--- gramps/plugins/tool/testcasegenerator.py
+++ gramps/plugins/tool/testcasegenerator.py
@@ -131,15 +131,11 @@ WIKI_HELP_SEC = _("Generate_Testcases_for_Persons_and_Families")
 # number generator.  The access is typically used for seeding the generator
 # to make it repeatable across runs.  The private copy is unaffected by other
 # uses of the global random() functions.
-try:
-    from gramps.gen.const import myrand
-except (NameError, ImportError):
-    myrand = random.Random()
-except:
-    print("Unexpected error:", sys.exc_info()[0])
-_random = myrand.random
-_choice = myrand.choice
-_randint = myrand.randint
+from gramps.gen.const import TEST_RANDOM
+
+_random = TEST_RANDOM.random
+_choice = TEST_RANDOM.choice
+_randint = TEST_RANDOM.randint
 
 
 LDS_ORD_BAPT_STATUS = (
